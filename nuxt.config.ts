94c3bab1fe71c973046c58cf507b3d6468cbfea9
kamatil-dev <karim.amahtil@gmail.com>
2024-01-01 22:47:58 +0100

    dev: add @nuxthq/studio

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: false },
-  modules: ["@nuxt/content", "@unocss/nuxt", "nuxt-simple-robots"],
+  modules: [
+    "@nuxthq/studio",
+    "@nuxt/content",
+    "@unocss/nuxt",
+    "nuxt-simple-robots",
+  ],
   site: { indexable: false },
   typescript: {
     shim: false,
